d35dff7a46c67abc41c143264063d53dce913c4b
minhuet <109837037+minh-1129@users.noreply.github.com>
2023-12-09 20:06:20 +0700

    commit

---
+++ UI/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module ui.ui {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens ui.ui to javafx.fxml;
+    exports ui.ui;
+}
